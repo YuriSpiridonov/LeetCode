7f118a3cd6ba9ce884d5f5bad86a4b20c2875221
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2020-07-24 20:38:48 +0300

    Update 1357.ApplyDiscountEverynOrders(BruteForce).py

--- Medium/1357.ApplyDiscountEverynOrders(BruteForce).py
+++ Medium/1357.ApplyDiscountEverynOrders(BruteForce).py
@@ -84,3 +84,8 @@ class Cashier:
             self.count = 0
             bill -= bill * self.discount / 100
         return bill
+
+
+# Your Cashier object will be instantiated and called as such:
+# obj = Cashier(n, discount, products, prices)
+# param_1 = obj.getBill(product,amount)
