846d8259fca192f69e8a42a3c5ad75d43df22fc1
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2020-08-01 19:12:08 +0300

    832. Flipping an Image
    Difficulty: Easy
    82 / 82 test cases passed.
    Runtime: 48 ms
    Memory Usage: 13.9 MB

---
+++ Easy/832.FlippinganImage(reverse).py
@@ -0,0 +1,33 @@
+"""
+    Given a binary matrix A, we want to flip the image horizontally, then 
+    invert it, and return the resulting image.
+    To flip an image horizontally means that each row of the image is reversed.  
+    For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+    To invert an image means that each 0 is replaced by 1, and each 1 is 
+    replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+
+    Example:
+    Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+    Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+    Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+    Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+
+    Notes:
+        - 1 <= A.length = A[0].length <= 20
+        - 0 <= A[i][j] <= 1
+"""
+#Difficulty: Easy
+#82 / 82 test cases passed.
+#Runtime: 48 ms
+#Memory Usage: 13.9 MB
+
+#Runtime: 48 ms, faster than 92.68% of Python3 online submissions for Flipping an Image.
+#Memory Usage: 13.9 MB, less than 39.81% of Python3 online submissions for Flipping an Image.
+
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        for row in A:
+            row.reverse()
+            for i in range(len(row)):
+                row[i] = abs(row[i]-1)
+        return A
