0beecf9542111ea141c1ea884deb7451dcd7fb47
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2020-11-13 14:02:04 +0100

    1646. Get Maximum in Generated Array
    Difficulty: Easy
    101 / 101 test cases passed.
    Runtime: 28 ms
    Memory Usage: 14.1 MB

---
+++ Easy/1646.GetMaximuminGeneratedArray.py
@@ -0,0 +1,58 @@
+"""
+    You are given an integer n. An array nums of length n + 1 is generated in 
+    the following way:
+        - nums[0] = 0
+        - nums[1] = 1
+        - nums[2 * i] = nums[i] when 2 <= 2 * i <= n
+        - nums[2 * i + 1] = nums[i] + nums[i + 1] when 2 <= 2 * i + 1 <= n
+
+    Return the maximum integer in the array nums​​​.
+
+    Example:
+    Input: n = 7
+    Output: 3
+    Explanation: According to the given rules:
+                   nums[0] = 0
+                   nums[1] = 1
+                   nums[(1 * 2) = 2] = nums[1] = 1
+                   nums[(1 * 2) + 1 = 3] = nums[1] + nums[2] = 1 + 1 = 2
+                   nums[(2 * 2) = 4] = nums[2] = 1
+                   nums[(2 * 2) + 1 = 5] = nums[2] + nums[3] = 1 + 2 = 3
+                   nums[(3 * 2) = 6] = nums[3] = 2
+                   nums[(3 * 2) + 1 = 7] = nums[3] + nums[4] = 2 + 1 = 3
+                 Hence, nums = [0,1,1,2,1,3,2,3], and the maximum is 3.
+
+    Example:
+    Input: n = 2
+    Output: 1
+    Explanation: According to the given rules, the maximum between nums[0], 
+                 nums[1], and nums[2] is 1.
+
+    Example:
+    Input: n = 3
+    Output: 2
+    Explanation: According to the given rules, the maximum between nums[0], 
+                 nums[1], nums[2], and nums[3] is 2.
+
+    Constraints:
+        - 0 <= n <= 100
+"""
+#Difficulty: Easy
+#101 / 101 test cases passed.
+#Runtime: 28 ms
+#Memory Usage: 14.1 MB
+
+#Runtime: 28 ms, faster than 87.49% of Python3 online submissions for Get Maximum in Generated Array.
+#Memory Usage: 14.1 MB, less than 82.45% of Python3 online submissions for Get Maximum in Generated Array.    
+
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        ans = 0
+        arr = [0, 1] + [0] * (n-1)
+        for i in range(1, n+1):
+            if i % 2:
+                arr[i] = arr[i//2] + arr[i//2+1]
+            else:
+                arr[i] = arr[i//2]
+            ans = max(ans, arr[i])
+        return ans
