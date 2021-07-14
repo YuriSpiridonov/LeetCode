2dfcc9c71466ffa2ebc1c89e461ddfca92e2e781
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2021-07-14 13:42:21 +0200

    Update 791.CustomSortString.py

--- Medium/791.CustomSortString.py
+++ Medium/791.CustomSortString.py
@@ -3,10 +3,10 @@
     In order, no letter occurs more than once.
 
     order was sorted in some custom order previously. We want
-     to permute the characters of str so that they match the 
-     order that order was sorted. More specifically, if x 
-     occurs before y in order, then x should occur before y 
-     in the returned string.
+    to permute the characters of str so that they match the 
+    order that order was sorted. More specifically, if x 
+    occurs before y in order, then x should occur before y 
+    in the returned string.
 
     Return any permutation of str (as a string) that satisfies 
     this property.
